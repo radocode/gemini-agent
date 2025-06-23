49c751aa54605578a395b49590c38df6b3d12244
Francisco González <francisco.gonzalez.lorca@gmail.com>
2025-06-23 15:34:44 +0000

    moar progress

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -30,3 +30,25 @@ def get_files_info(working_directory, directory=None):
 
     except Exception as e:
         return f"Error: {e}"
+
+def get_file_content(working_directory, file_path):
+    try:
+        MAX_CHARS = 10000
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        if not abs_target_path.startswith(abs_working_dir):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(abs_target_path):
+            return f'Error: "{file_path}" is not a file'
+
+        with open(abs_target_path, "r", encoding="utf-8") as f:
+            content = f.read(MAX_CHARS + 1)
+            if len(content) > MAX_CHARS:
+                content = content[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            return content
+
+    except Exception as e:
+        return f"Error: {e}"
+        
